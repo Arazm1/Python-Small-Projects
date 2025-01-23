8dccba79a020e5342c3ff7e741f66c63c8c9b068
Arazm1 <arazm@metropolia.fi>
2025-01-23 09:23:03 +0200

    Update and rename version1.py to version1.py

--- ExchangeRates/version2.py
+++ ExchangeRates/version2.py
@@ -5,9 +5,14 @@ import requests
 Your_Api_Key = ""
 
 input_currency = input("Enter the currency you want to convert from: ").upper()
+original_amount_currency = float(input(f"Enter the amount of {input_currency} you wish to convert: "))
 converted_currency = input("Enter the currency you want to convert to: ").upper()
 
 
 response = f"https://v6.exchangerate-api.com/v6/{Your_Api_Key}/latest/{input_currency}"
 data = requests.get(response).json()
-print(f"1 {input_currency} is {data['conversion_rates'][converted_currency]} {converted_currency}")
+
+
+conversion_rate = data['conversion_rates'][converted_currency]
+converted_amount = original_amount_currency * conversion_rate
+print(f"{original_amount_currency} {input_currency} is {converted_amount:.3f} {converted_currency}")
